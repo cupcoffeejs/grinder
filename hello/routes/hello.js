e5d3c5817bbb0c6afd085ee940fe27d64e7a1491
Philippe Assis <assis@phlippeassis.com>
2016-07-05 12:02:01 -0300

    up

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -1,10 +1,9 @@
 "use strict";
-var router = require('express').Router(),
-    controller = require('cupcoffee').controller
 
-router.get('/', (request, response) => {
-    controller.http(request, response).invoke('hello', 'index')
-});
+module.exports = ({router, controller}) => {
+    router.get('/helloword', (request, response) => {
+        controller.http(request, response).invoke('hello', 'index')
+    });
 
-
-module.exports = router;
+    return router;
+};
